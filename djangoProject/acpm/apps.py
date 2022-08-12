207861b41c94c3363adca4d83ea5b5434247d643
kekaisik <102830595+kekaisik@users.noreply.github.com>
2022-08-12 18:54:45 +0600

    Delete djangoProject directory

--- djangoProject/acpm/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AcpmConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'acpm'
--- djangoProject/acpm/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Post
-
-
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ("title", "text", "main_image", "images_set")
